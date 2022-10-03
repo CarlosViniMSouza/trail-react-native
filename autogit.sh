f8682ecf38f056bbd1be25b106ffc1ba169f5cb9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-10-03 15:16:38 +0000

    expo for web -> installed | Env. Codespace configured

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,4 @@
 git add .
-git commit -m "Project insert on github"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/trail-react-native.git
+git commit -m "expo for web -> installed | Env. Codespace configured"
 git push -u origin main
 git status
