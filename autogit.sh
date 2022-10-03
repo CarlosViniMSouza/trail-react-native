8352c4b230153e464dec997f93fa65fa0b8ef533
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-10-03 12:00:43 -0300

    Project insert on github

---
+++ autogit.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "Project insert on github"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/trail-react-native.git
+git push -u origin main
+git status
